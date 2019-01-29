ab48e82d5d6478ba60d11b2206291475251b739e
tera3939 <tera3939.dev@gmail.com>
2019-01-29 19:15:08 +0900

    データベース設定するぞ

--- config.py
+++ config.py
@@ -1,14 +1,47 @@
+from pymongo import MongoClient
 from mastodon import Mastodon
 
 
 MASTODON_HOST = "mastodon.cloud"
 BASE_URL = "https://" + MASTODON_HOST
 APP_NAME = "neko-miru"
+SECRET_NAME = APP_NAME+".secret"
+
+DATABASE_NAME = "neko-miru"
+TOOT_COLLECTION = "toots"
+FAVORITES_COLLECTION = "favorites"
+
+
+def create_app():
+    global MASTODON_HOST
+    global BASE_URL
+    global SECRET_NAME
+
+    Mastodon.create_app(
+        APP_NAME,
+        api_base_url=BASE_URL,
+        to_file=SECRET_NAME
+    )
+
+
+def create_db():
+    global DATABASE_NAME
+    global TOOT_COLLECTION
+    global FAVORITES_COLLECTION
+
+    con = MongoClient()
+    db = con[DATABASE_NAME]
+    db.create_collection(TOOT_COLLECTION, capped=True, size=1000000)
+    db.create_collection(FAVORITES_COLLECTION)
+    db[TOOT_COLLECTION].create_index([("id", 1)], unique=True)
+    db[FAVORITES_COLLECTION].create_index([("id", 1)], unique=True)
+    con.close()
+
+
+def main():
+    create_app()
+    create_db()
 
 
 if __name__ == "__main__":
-    Mastodon.create_app(
-     APP_NAME,
-     api_base_url=BASE_URL,
-     to_file=APP_NAME+'.secret'
-    )
+    main()
